58b03d51ed95d05bf827559f549635d37c5ae74a
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2020-10-13 09:59:59 -0300

    Adcionanndo as especificacoes de lexemas usando regex.

--- lex.py
+++ lex.py
@@ -12,5 +12,16 @@ def lex(code: str) -> Iterable[Token]:
     Retorna sequência de objetos do tipo token correspondendo à análise léxica
     da string de código fornecida.
     """
+    token_lexemas = [
+        ("STRING", r"\".*\""),
+        ("NUMBER", r"(\+|\-|)\d+(\.\d*)?"),
+        ("NAME", r"([a-zA-Z_%\+\-]|\.\.\.)[a-zA-Z_0-9\-\>\?\!]*"),
+        ("CHAR", r"#\\[a-zA-Z]*"),
+        ("QUOTE", r"\'"),
+        ("LPAR", r"\("),
+        ("RPAR", r"\)"),
+        ("BOOL", r"#[t|f]")
+    ]
+    
 
     return [Token('INVALIDA', 'valor inválido')]
